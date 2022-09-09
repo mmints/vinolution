fc69a040a67ab36002844e4632a4fd1bf595eb22
Mark Oliver Mints <mmints@uni-koblenz.de>
2022-09-09 11:13:14 +0200

    upadte experiment to work on hill and hill erosion

--- plane_sampling.py
+++ plane_sampling.py
@@ -2,24 +2,22 @@
 
 import open3d as o3d
 
-simple_plane = o3d.io.read_triangle_mesh("meshes/simple-plane.ply")
-simple_plane.compute_vertex_normals()
+hill = o3d.io.read_triangle_mesh("meshes/simple-hill.ply")
+hill.compute_vertex_normals()
 
-simple_hill = o3d.io.read_triangle_mesh("meshes/simple-hill-rinnen-combined.ply")
-simple_hill.compute_vertex_normals()
-
-
-o3d.visualization.draw_geometries([simple_plane])
-o3d.visualization.draw_geometries([simple_hill])
+hill_erosion = o3d.io.read_triangle_mesh("meshes/simple-hill-erosion.ply")
+hill_erosion.compute_vertex_normals()
 
 
-simple_plane_pc = simple_plane.sample_points_uniformly(number_of_points=5000)
-simple_hill_pc = simple_hill.sample_points_uniformly(number_of_points=5000)
+o3d.visualization.draw_geometries([hill])
+o3d.visualization.draw_geometries([hill_erosion])
 
 
-simple_plane_pc.paint_uniform_color([1, 0.706, 0])
-simple_hill_pc.paint_uniform_color([0, 0.651, 0.929])
+hill_pc = hill.sample_points_uniformly(number_of_points=5000)
+hill_erosion_pc = hill_erosion.sample_points_uniformly(number_of_points=5000)
 
 
-o3d.visualization.draw_geometries([simple_hill_pc, simple_plane_pc])
+hill_pc.paint_uniform_color([1, 0.706, 0])
+hill_erosion_pc.paint_uniform_color([0, 0.651, 0.929])
 
+o3d.visualization.draw_geometries([hill_pc, hill_erosion_pc])
