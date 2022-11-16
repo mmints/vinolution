b5f409fe7b294197e50aa604a86b600c5cec6d3e
Mark Oliver Mints <mmints@uni-koblenz.de>
2022-11-16 22:26:13 +0100

    update plane sample

--- prototyping/plane_sampling.py
+++ prototyping/plane_sampling.py
@@ -5,10 +5,10 @@ import open3d as o3d
 import numpy as np
 
 hill = o3d.io.read_triangle_mesh("../meshes/simple-hill.ply")
-hill.compute_vertex_normals()
+# hill.compute_vertex_normals()
 
 hill_erosion = o3d.io.read_triangle_mesh("../meshes/simple-hill-erosion3.ply")
-hill_erosion.compute_vertex_normals()
+# hill_erosion.compute_vertex_normals()
 
 hill_pc = hill.sample_points_uniformly(number_of_points=30000)
 hill_erosion_pc = hill_erosion.sample_points_uniformly(number_of_points=30000)
@@ -16,12 +16,13 @@ hill_erosion_pc = hill_erosion.sample_points_uniformly(number_of_points=30000)
 hill_pc.paint_uniform_color([1, 0.706, 0])
 hill_erosion_pc.paint_uniform_color([0, 0.651, 0.929])
 
-#o3d.visualization.draw_geometries([hill_pc, hill_erosion_pc])
+o3d.visualization.draw_geometries([hill_pc])
+o3d.visualization.draw_geometries([hill_erosion_pc])
 
 dists = hill_erosion_pc.compute_point_cloud_distance(hill_pc)
 #dists = np.asarray(dists)
-print(np.asarray(dists).size)
-print(np.asarray(hill_pc.points))
+# print(np.asarray(dists).size)
+# print(np.asarray(hill_pc.points))
 
 difference_idx = np.argwhere(np.asarray(dists) >= 0.1)
 print(np.asarray(difference_idx).size)
@@ -31,22 +32,42 @@ print(difference_idx)
 difference_pc = hill_erosion_pc.select_by_index(difference_idx)
 difference_pc.paint_uniform_color([1.0, 0.0, 0.0])
 
+dists2 = hill_pc.compute_point_cloud_distance(hill_erosion_pc)
+#dists = np.asarray(dists)
+# print(np.asarray(dists2).size)
+# print(np.asarray(hill_pc.points))
+
+difference_idx2 = np.argwhere(np.asarray(dists2) >= 0.1)
+print(np.asarray(difference_idx2).size)
+
+print(difference_idx2)
+
+difference_pc2 = hill_pc.select_by_index(difference_idx2)
+difference_pc2.paint_uniform_color([0.0, 1.0, 0.0])
+
+
+o3d.visualization.draw_geometries([difference_pc])
+o3d.visualization.draw_geometries([difference_pc2])
 
 print("Start outlier removal with nb_neighbors")
 cl, ind = difference_pc.remove_radius_outlier(nb_points = 10, radius = 0.25, print_progress = True)
 print("remove_outlier: DONE!")
-
-print(len(ind))
-
 difference_pc = difference_pc.select_by_index(ind)
 
-o3d.visualization.draw_geometries([hill])
-o3d.visualization.draw_geometries([hill_erosion])
+print("Start outlier removal with nb_neighbors")
+cl, ind = difference_pc2.remove_radius_outlier(nb_points = 10, radius = 0.25, print_progress = True)
+print("remove_outlier: DONE!")
+difference_pc2 = difference_pc2.select_by_index(ind)
 
-o3d.visualization.draw_geometries([hill_pc, hill_erosion_pc, difference_pc])
-o3d.visualization.draw_geometries([hill_pc, difference_pc])
+# o3d.visualization.draw_geometries([hill])
+# o3d.visualization.draw_geometries([hill_erosion])
 
-o3d.visualization.draw_geometries([hill_erosion_pc, difference_pc])
-o3d.visualization.draw_geometries([hill, difference_pc])
+# o3d.visualization.draw_geometries([hill_pc, hill_erosion_pc, difference_pc])
+# o3d.visualization.draw_geometries([hill_pc, difference_pc])
+
+# o3d.visualization.draw_geometries([hill_erosion_pc, difference_pc])
+# o3d.visualization.draw_geometries([hill, difference_pc])
 
 o3d.visualization.draw_geometries([difference_pc])
+o3d.visualization.draw_geometries([difference_pc2])
+o3d.visualization.draw_geometries([difference_pc, difference_pc2])
